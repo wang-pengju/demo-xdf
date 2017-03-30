985f9b5889df7544fb43b36694cfceb142b111e7
wang-pengju <526527853@qq.com>
2017-03-30 12:59:09 +0800

    Some features are tested

--- routes/index.js
+++ routes/index.js
@@ -3,38 +3,26 @@ var router = express.Router();
 var mysql = require('mysql');
 var connection = require('./db.js');
 
-var fs = require('fs');
 
 //插入新闻
 router.post('/', function(req, res) {
+    connection.connect();
     var userName = req.body.userName,
         userEmail = req.body.userEmail,
         userTel = req.body.userTel,
         userTime = req.body.userTime;
-    console.log(userName);
-    console.log(userEmail);
-    console.log(userTel);
-    console.log(userTime);
-    // connection.query('INSERT INTO `news` (`userName`,`userEmail`,`userTel`,`userTime`) VAlUES (?,?,?,?)', [userName, userEmail, userTel, userTime], function(err, result, fields) {
-    //     if (err) {
-    //         console.log('insert error');
-    //         return;
-    //     }
-    //     console.log(result.insertId);
-    //     res.end('ok');
-    // });
-    var rows = (JSON.parse(fs.readFileSync('./data.json')));
-    console.log(rows);
-    var useid = rows.rows.length;
-    rows.rows.push({
-        id: useid,
-        userName: userName,
-        userEmail: userEmail,
-        userTel: userTel,
-        userTime: userTime
+    /* console.log(userName);
+     console.log(userEmail);
+     console.log(userTel);
+     console.log(userTime);*/
+    connection.query('INSERT INTO `news` (`userName`,`userEmail`,`userTel`,`userTime`) VAlUES (?,?,?,?)', [userName, userEmail, userTel, userTime], function(err, result, fields) {
+        if (err) {
+            console.log('insert error');
+            return;
+        }
+        console.log(result.insertId);
+        res.end('ok');
     });
-    fs.writeFileSync('./data.json', JSON.stringify(rows));
-    res.end('ok');
 });
 
 
